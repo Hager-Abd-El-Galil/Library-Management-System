1c1de5ad7aa5ef660835ea223d56b02657299f1c
Hager Abd El Galil <Hager.galil66@gmail.com>
2024-10-12 15:27:00 +0300

    Add Auditing to Library

--- src/main/java/com/system/library/auditing/config/AuditorAwareImpl.java
+++ src/main/java/com/system/library/auditing/config/AuditorAwareImpl.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.system.library.security.entity.UserCredentials;
 
 @Configuration
+@EnableJpaAuditing
 public class AuditorAwareImpl implements AuditorAware<UserCredentials> {
 
 	@Override
